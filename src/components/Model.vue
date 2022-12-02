ac17fdb5fd5cb5d847c768a3ba1d7edf7afdc2b2
Rukshan <arkruka@gmail.com>
2022-12-02 23:24:20 +0530

    model component initial commit

---
+++ src/components/Model.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    v-if="visible"
+    class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0"
+  >
+    <div class="bg-white px-16 py-14 rounded-md text-center">
+      <h1 class="text-xl mb-1 font-bold text-slate-500">Confirm your action</h1>
+      <p class="font-thin text-slate-700">
+        In order to confirm your acction type
+        <span class="font-bold uppercase">{{ message }}</span> and press
+        confirm.
+      </p>
+      <input
+        type="text"
+        v-model="userInput"
+        class="block w-full mb-2 rounded-md px-3 py-2 border-2 mt-1 focus:outline-none focus:border-teal-600"
+      />
+      <button
+        @click="hide"
+        class="bg-red-500 px-4 py-2 rounded-md text-md text-white"
+      >
+        Cancel
+      </button>
+      <button
+        @click="confirmAction"
+        class="bg-indigo-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
+      >
+        Confirm
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+
+export default defineComponent({
+  props: ["message", "callbackReturn", "visible"],
+  emits: ["callbackEvent"],
+  setup(props, ctx) {
+    onMounted(() => {
+      visible.value = props.visible;
+    });
+
+    let visible = ref(true);
+    let userInput = ref("");
+    const confirmAction = () => {
+      if (
+        (props.message as string).toLowerCase() ===
+        userInput.value.toLowerCase()
+      ) {
+        ctx.emit("callbackEvent", props.callbackReturn);
+        hide();
+      }
+    };
+
+    const hide = () => {
+      visible.value = false;
+    };
+
+    return {
+      confirmAction,
+      userInput,
+      hide,
+      visible,
+    };
+  },
+});
+</script>
