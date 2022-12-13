c7e491da00eac8e8a5b5b2be3268e639cb836f73
Rukshan <arkruka@gmail.com>
2022-12-13 09:14:09 +0530

    update to vanilla components

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,112 +17,134 @@
           <label for="email" class="block font-semibold text-gray-700"
             >Email</label
           >
-          <input
+          <Input
+            placeholder="Email Address"
+            type="email"
+            required
+            v-model="email"
+            :errors="inputEmailError"
+            :hasErrors="hasInputEmailError"
+          />
+          <!-- <input
             v-model="email"
             placeholder="Email Address"
             type="email"
             class="rounded w-full px-2 py-1.5 border-2 focus:border-teal-600 focus:outline-none transition ease-in-out duration-150"
             required
-          />
+          /> -->
         </div>
         <div class="control space-y-1.5">
           <label for="password" class="block font-semibold text-gray-700"
             >Password</label
           >
-          <input
+          <!-- <input
             v-model="password"
             type="password"
             placeholder="Password"
             class="rounded w-full px-2 py-1.5 border-2 focus:border-teal-600 focus:outline-none transition ease-in-out duration-150"
             required
+          /> -->
+          <Input
+            placeholder="Password"
+            v-model="password"
+            required
+            type="password"
+            :errors="inputPasswordError"
+            :hasErrors="hasInputPasswordError"
           />
         </div>
-        <button
+        <!-- <button
           @click.prevent="login()"
           :disabled="isLoading"
           class="rounded-md px-3 py-2 font-bold bg-teal-600 hover:bg-teal-700 w-32 text-white focus:outline-none disabled:bg-teal-500"
         >
           Login
-        </button>
+        </button> -->
+        <Button
+          @click.prevent="login()"
+          label="Login"
+          variant="primary"
+          :loading="isLoading"
+          :disabled="isLoading"
+        />
       </form>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent, ref } from "vue";
 import Notification from "@/components/Notification.vue";
 import LoadingBar from "@/components/LoadingBar.vue";
 import { KeyStore } from "@/store/keyStore";
 import { UserStore } from "@/store/userStore";
 import { useRouter } from "vue-router";
+import { Input, Button } from "@flavorly/vanilla-components";
 
-export default defineComponent({
-  setup() {
-    const router = useRouter();
-    const keyStore = KeyStore();
-    const userStore = UserStore();
+const router = useRouter();
+const keyStore = KeyStore();
+const userStore = UserStore();
 
-    const password = ref(undefined);
-    const email = ref(undefined);
-    const hasNotification = ref(false);
-    const notificationMessage = ref("");
-    const notificationType = ref("success");
-    const isLoading = ref(false);
+const password = ref(undefined);
+const email = ref(undefined);
+const hasNotification = ref(false);
+const notificationMessage = ref("");
+const notificationType = ref("success");
+const isLoading = ref(false);
+const inputPasswordError = ref("");
+const inputEmailError = ref("");
+const hasInputEmailError = ref(false);
+const hasInputPasswordError = ref(false);
 
-    const login = async () => {
-      if (!email.value || !password.value) {
-        hasNotification.value = true;
-        notificationMessage.value =
-          "An email address and a password is required";
-        notificationType.value = "warning";
-        return;
+const login = async () => {
+  if (!email.value || !password.value) {
+    hasInputEmailError.value = true;
+    hasInputPasswordError.value = true;
+    inputEmailError.value = "Email is requred";
+    inputPasswordError.value = "Password is required";
+    notificationType.value = "warning";
+    return;
+  }
+  isLoading.value = true;
+  try {
+    const request = await fetch(
+      `${import.meta.env.VITE_SERVER_URL}/user/login`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email.value,
+          password: password.value,
+        }),
       }
-      isLoading.value = true;
-      const request = await fetch(
-        `${import.meta.env.VITE_SERVER_URL}/user/login`,
-        {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email.value,
-            password: password.value,
-          }),
-        }
-      );
+    );
 
-      if (!request.ok) {
-        hasNotification.value = true;
-        notificationMessage.value =
-          "An error occured while logging in, please try again";
-      }
+    if (!request.ok) {
+      hasNotification.value = true;
+      notificationMessage.value =
+        "An error occured while logging in, please try again";
+    }
 
-      if (request.status === 400) {
-        hasNotification.value = true;
-        notificationMessage.value = "Invalid username or password";
-      }
+    if (request.status === 400) {
+      hasNotification.value = true;
+      notificationMessage.value = "Invalid username or password";
+    }
 
-      if (request.status === 200) {
-        const payload = await request.json();
-        keyStore.setKey(payload.data.token);
-        userStore.setUser(payload.data.user.name, payload.data.user.email);
-        document.cookie = `token=${payload.data.token};max-age=7200;path=/`;
-        return router.push({ name: "Dashboard" });
-      }
-      isLoading.value = false;
-    };
-    return {
-      email,
-      password,
-      login,
-      hasNotification,
-      notificationMessage,
-      notificationType,
-      isLoading,
-    };
-  },
-  components: { Notification, LoadingBar },
-});
+    if (request.status === 200) {
+      const payload = await request.json();
+      keyStore.setKey(payload.data.token);
+      userStore.setUser(payload.data.user.name, payload.data.user.email);
+      document.cookie = `token=${payload.data.token};max-age=7200;path=/`;
+      return router.push({ name: "Dashboard" });
+    }
+    isLoading.value = false;
+  } catch (error) {
+    hasNotification.value = true;
+    notificationMessage.value =
+      "An error occured while logging in, please try again";
+    isLoading.value = false;
+  }
+};
 </script>
